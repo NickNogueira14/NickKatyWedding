c0e7dfdb87eac23c4a619e41a9d9d6428ed94cc9
Vinícius Nogueira <Vincius Nogueira>
2017-02-23 23:37:51 -0300

    Criacao da pagina de Fotos

--- src/main/webapp/ctrl/PictureUploadCtrl.js
+++ src/main/webapp/ctrl/PictureUploadCtrl.js
@@ -1,35 +1,60 @@
-WeddingApp.controller('PictureUploadCtrl', ['$scope', function($scope) {
+WeddingApp.controller('PictureUploadCtrl', ['$scope', '$mdDialog', function($scope, $mdDialog) {
 	
-	$scope.fileName = '';
+	$scope.photoList = [
+	                    {index : 0, path : '../images/pictures/picture01.jpg'},
+	                    {index : 1, path : '../images/pictures/picture02.jpg'},
+	                    {index : 2, path : '../images/pictures/picture03.jpg'},
+	                    {index : 3, path : '../images/pictures/picture04.jpg'},
+	                    {index : 4, path : '../images/pictures/picture05.jpg'},
+	                    {index : 5, path : '../images/pictures/picture06.jpg'},
+	                    {index : 6, path : '../images/pictures/picture07.jpg'},
+	                    {index : 7, path : '../images/pictures/picture08.jpg'},
+	                    {index : 8, path : '../images/pictures/picture09.jpg'},
+	                    {index : 9, path : '../images/pictures/picture10.jpg'}
+	                    ];
 	
-	$scope.selectFile = function(element, attrs) {
-		document.getElementById('textInput').click();
-	};
-	
-	$scope.getFileName = function() {
-		var input = document.getElementById('textInput');
-		$scope.fileName = document.getElementById('textInput').files[0].name;
-		console.log($scope.fileName);
-		
-		if(document.getElementById('textInput').files[0].name != '' 
-			|| document.getElementById('textInput').files[0].name != null) {
-			
-			var fReader = new FileReader();
-			
-			fReader.onload = function(){
+	$scope.picture = null;
 
-				var img = document.getElementById("imageSelected");
-				img.onload = function() {
-					$scope.imgWidth = img.width;
-					$scope.imgHeight = img.height;
-					
-					$scope.$apply();
-				};
-				img.src = fReader.result;
-			}
-			fReader.readAsDataURL(input.files[0]);
+	$scope.showPhotoBig = function(ev, pic) {
+		$scope.picture = angular.copy(pic);
+		
+	    $mdDialog.show({
+	    	scope: $scope.$new(),
+	    	templateUrl: 'pages/pictureModal.html',
+	    	parent: angular.element(document.body),
+	    	targetEvent: ev,
+	    	clickOutsideToClose:true,
+	    	fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+	    });
+	}
+	
+	$scope.nextPicture = function(evt) {
+		
+		$mdDialog.hide();
+		
+		var index = $scope.picture.index;
+		if(index < $scope.photoList.length - 1) {
+			var photo = $scope.photoList[index + 1];
+		} else {
+			var photo = $scope.photoList[0];
 		}
 		
-		$scope.$apply();
+		$scope.showPhotoBig(evt, photo);
 	}
+	
+	$scope.previousPicture = function(evt) {
+		
+		$mdDialog.hide();
+		
+		var index = $scope.picture.index;
+		if(index > 0) {
+			var photo = $scope.photoList[index - 1];
+		} else {
+			index = $scope.photoList.length - 1;
+			var photo = $scope.photoList[index];
+		}
+		
+		$scope.showPhotoBig(evt, photo);
+	}
+	
 }]);
